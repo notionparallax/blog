62f794cfea4193eb83e4f4ff57955ce6cd32107f
Ben Doherty <ben@notionparallax.co.uk>
2016-06-13 19:11:16 -0700

    should work now

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,14 @@
 // layout: null
 //---
 
+var staticCacheName = 'np-v5';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('np-v2').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
+        '/index-data.json',
         '/css/main.css',
         '/js/ga-events.js',
         'https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js',
@@ -15,18 +18,55 @@ self.addEventListener('install', function(event) {
         'https://fonts.gstatic.com/s/lustria/v4/P1zvvbOqCVCJzgypDg3MKw.woff2',
         'https://fonts.gstatic.com/s/opensans/v13/DXI1ORHCpsQm3Vp6mXoaTegdm0LZdjqr5-oayXSOefg.woff2',
         'https://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzOgdm0LZdjqr5-oayXSOefg.woff2',
-        '/js/loadRemainingContent.js',
-        '/index-data.json'
+        '/js/loadRemainingContent.js'
       ]);
     })
   );
 });
 
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       //console.log("getting ", event.request, response);
+//       setTimeout(otherstuff, 0);
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      //console.log("getting ", event.request, response);
-      return response || fetch(event.request);
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {
+          // if we got a response from the cache, update the cache
+          if (response) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        });
+
+        // respond from the cache, or the network
+        return response || fetchPromise;
+      });
+    })
+  );
+});
+
+
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('np-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          console.log("deleting " + cacheName);
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
