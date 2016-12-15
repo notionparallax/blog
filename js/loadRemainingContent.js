dc35377acc5b56b885e834b6b60303bbc5c4116f
Ben Doherty <ben@notionparallax.co.uk>
2016-12-16 09:08:41 +1100

    refactor

--- js/loadRemainingContent.js
+++ js/loadRemainingContent.js
@@ -1,30 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var url = "/index-data.json";
 
-    function makeRequest (method, url) {
-      return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function () {
-          if (this.status >= 200 && this.status < 300) {
-            resolve(xhr.response);
-          } else {
-            reject({
-              status: this.status,
-              statusText: xhr.statusText
-            });
-          }
-        };
-        xhr.onerror = function () {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        };
-        xhr.send();
-      });
-    }
-
     makeRequest("GET", url)
         .then(function (datums) {
           // console.log(datums);
@@ -38,40 +14,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
         console.log(theJSON[0]);
         let newInnerHTML = "";
 
-        // for(let p of theJSON) {
-        //   let liTemplate = `
-        //     <li itemscope itemtype="http://schema.org/BlogPosting">
-        //       <span class="post-date">${p.pretty_date || " "}</span>
-        //       <meta itemprop="datePublished" content="${p.date || " "}" />
-        //       <meta itemprop="dateModified" content="${p.date || " "}" />
-        //       <span itemprop="author" itemscope itemtype="http://schema.org/Person">
-        //         <span itemprop="name" content="${p.author || " "}"></span>
-        //       </span>
-        //       <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-        //         <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-        //           <meta itemprop="url" content="https://upload.wikimedia.org/wikipedia/commons/9/9e/Sus_scrofa_avionica.png">
-        //           <meta itemprop="width"  content="700">
-        //           <meta itemprop="height" content="500">
-        //         </span>
-        //       <meta itemprop="name" content="Notion Parallax">
-        //       </span>
-        //       <span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-        //         <meta itemprop="url" content="https://upload.wikimedia.org/wikipedia/commons/9/9e/Sus_scrofa_avionica.png">
-        //           <meta itemprop="width"  content="700">
-        //           <meta itemprop="height" content="500">
-        //       </span>
-        //       <h2 itemprop="headline">
-        //         <a itemprop="mainEntityOfPage"  class="post-link" href="${p.url || " "}">${p.title || " "}</a>
-        //       </h2>
-        //       <p itemprop="description">${p.description || ">_"}</p>
-        //       <a class="read-more" href="${p.url || " "}">read more</a>
-        //     </li>`
-        //   newInnerHTML += liTemplate;
-        // }
+        for(let p of theJSON) {
+          let liTemplate = `
+            <li itemscope itemtype="http://schema.org/BlogPosting">
+              <span class="post-date">${p.pretty_date || " "}</span>
+              <meta itemprop="datePublished" content="${p.date || " "}" />
+              <meta itemprop="dateModified" content="${p.date || " "}" />
+              <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+                <span itemprop="name" content="${p.author || " "}"></span>
+              </span>
+              <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+                <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+                  <meta itemprop="url" content="https://upload.wikimedia.org/wikipedia/commons/9/9e/Sus_scrofa_avionica.png">
+                  <meta itemprop="width"  content="700">
+                  <meta itemprop="height" content="500">
+                </span>
+              <meta itemprop="name" content="Notion Parallax">
+              </span>
+              <span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+                <meta itemprop="url" content="https://upload.wikimedia.org/wikipedia/commons/9/9e/Sus_scrofa_avionica.png">
+                  <meta itemprop="width"  content="700">
+                  <meta itemprop="height" content="500">
+              </span>
+              <h2 itemprop="headline">
+                <a itemprop="mainEntityOfPage"  class="post-link" href="${p.url || " "}">${p.title || " "}</a>
+              </h2>
+              <p itemprop="description">${p.description || ">_"}</p>
+              <a class="read-more" href="${p.url || " "}">read more</a>
+            </li>`
+          newInnerHTML += liTemplate;
+        }
 
-        // let postList = document.getElementsByClassName("post-list").item(0);
-        // postList.innerHTML = newInnerHTML;
-        // console.log("Added the extra posts");
+        let postList = document.getElementsByClassName("post-list").item(0);
+        postList.innerHTML = newInnerHTML;
+        console.log("Added the extra posts");
         console.log("this is just silly");
     }
 });
