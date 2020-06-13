43fadc15a8f7da1a27cfc4a2cbde6f445fc2a56a
Ben <ben@notionparallax.co.uk>
2020-06-13 15:45:28 +1000

    prettier

--- service-worker.js
+++ service-worker.js
@@ -4,24 +4,24 @@
 
 var staticCacheName = "np-v27";
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        "/",
-        "/index-data.json",
-        "/css/main.css",
-        "/js/ga-events.js",
-        "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/balance-text/1.6.0/jquery.balancetext.min.js",
-        "https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif",
-        "https://fonts.gstatic.com/s/notosans/v6/LeFlHvsZjXu2c3ZRgBq9nFtXRa8TVwTICgirnJhmVJw.woff2",
-        "https://fonts.gstatic.com/s/notoserif/v4/eCpfeMZI7q4jLksXVRWPQ_k_vArhqVIZ0nv9q090hN8.woff2",
-        "https://fonts.gstatic.com/s/notoserif/v4/1_daFS3X6gkNOcmGmHl7UiEAvth_LlrfE80CYdSH47w.woff2",
-        "/js/loadRemainingContent.js"
-      ]);
-    })
-  );
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.addAll([
+                "/",
+                "/index-data.json",
+                "/css/main.css",
+                "/js/ga-events.js",
+                "https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/balance-text/1.6.0/jquery.balancetext.min.js",
+                "https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Serif",
+                "https://fonts.gstatic.com/s/notosans/v6/LeFlHvsZjXu2c3ZRgBq9nFtXRa8TVwTICgirnJhmVJw.woff2",
+                "https://fonts.gstatic.com/s/notoserif/v4/eCpfeMZI7q4jLksXVRWPQ_k_vArhqVIZ0nv9q090hN8.woff2",
+                "https://fonts.gstatic.com/s/notoserif/v4/1_daFS3X6gkNOcmGmHl7UiEAvth_LlrfE80CYdSH47w.woff2",
+                "/js/loadRemainingContent.js",
+            ]);
+        })
+    );
 });
 
 // self.addEventListener('fetch', function(event) {
@@ -34,38 +34,43 @@ self.addEventListener("install", function(event) {
 //   );
 // });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          // if we got a response from the cache, update the cache
-          if (response) {
-            cache.put(event.request, networkResponse.clone());
-          }
-          return networkResponse;
-        });
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.open(staticCacheName).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                var fetchPromise = fetch(event.request).then(function (
+                    networkResponse
+                ) {
+                    // if we got a response from the cache, update the cache
+                    if (response) {
+                        cache.put(event.request, networkResponse.clone());
+                    }
+                    return networkResponse;
+                });
 
-        // respond from the cache, or the network
-        return response || fetchPromise;
-      });
-    })
-  );
+                // respond from the cache, or the network
+                return response || fetchPromise;
+            });
+        })
+    );
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function(cacheName) {
-            return cacheName.startsWith("np-") && cacheName != staticCacheName;
-          })
-          .map(function(cacheName) {
-            console.log("deleting " + cacheName);
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function (cacheName) {
+                        return (
+                            cacheName.startsWith("np-") &&
+                            cacheName != staticCacheName
+                        );
+                    })
+                    .map(function (cacheName) {
+                        console.log("deleting " + cacheName);
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
 });
